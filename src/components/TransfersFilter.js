c69924b4e83a37a410fed6af8d3107941c5b1ff8
Madina <a.madikosh2004@gmail.com>
2025-01-06 23:48:10 +0500

    commit

---
+++ src/components/TransfersFilter.js
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+import { Card, List, Checkbox, Divider, Typography  } from 'antd';
+
+const { Group: CheckboxGroup } = Checkbox;
+
+const plainOptions =[ "1 пересадка", "2 пересадки", "3 пересадки"];
+const defaultcheckedList = ["1 пересадка", "2 пересадки"];
+const TranfersFilter = () => {
+    const [checkedList, setCheckedList] = useState(defaultcheckedList);
+    const checkAll = plainOptions.length === checkedList.length;
+    const indeterminate = checkedList.length > 0  && checkedList.length < plainOptions.length;
+    const onChange = (list) => {
+        setCheckedList(list);
+    }
+    const onCheckAllChange = (e) => {
+        setCheckedList(e.target.checked ? plainOptions : []);
+    }
+
+    const data = [...plainOptions];
+    return <div >
+        {/* <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
+            Все
+        </Checkbox>
+        <Divider />
+      <CheckboxGroup options={plainOptions} value={checkedList} onChange={onChange} /> */}
+      
+      <List
+        header={<Checkbox
+            indeterminate={indeterminate}
+            onChange={onCheckAllChange}
+            checked={checkAll}
+          >
+            Все
+          </Checkbox>}
+          style={{
+            width: "300px"
+          }}
+       
+        bordered
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item>
+            <Checkbox
+              checked={checkedList.includes(item)}
+              onChange={(e) =>
+                onChange(
+                  e.target.checked
+                    ? [...checkedList, item]
+                    : checkedList.filter((i) => i !== item)
+                )
+              }
+            >
+              {item}
+            </Checkbox>
+          </List.Item>
+        )}
+      />
+        
+  
+    </div>
+
+
+}
+export default TranfersFilter;
